7f3150f3295c4d38b354990701278af1bfd9025f
Dmitry <dmitry.r@cyberflow.net>
2015-02-09 16:26:40 +0200

    add name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
-maintainer       "VPSLab"
-maintainer_email "pawel.pacana@gmail.com"
-license          "Apache 2.0"
-description      "Installs libvirt and provides libvirt resources."
+name             'libvirt'
+maintainer       'VPSLab'
+maintainer_email 'pawel.pacana@gmail.com'
+license          'Apache 2.0'
+description      'Installs libvirt and provides libvirt resources.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.1"
+version          '0.2.1'
 
 %w(ubuntu debian).each do |os|
   supports os
