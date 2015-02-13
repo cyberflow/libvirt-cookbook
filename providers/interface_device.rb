b9a7c4bab40e0fb46cfffdbf356bf85e4f20ab30
Dmitry <dmitry.r@cyberflow.net>
2015-02-13 15:12:02 +0200

    fix

--- providers/interface_device.rb
+++ providers/interface_device.rb
@@ -5,33 +5,35 @@ def load_current_resource
 end
 
 action :attach do
-  begin
-    domain = load_domain
-    device_xml = Tempfile.new(new_resource.name)
-    t = template device_xml.path do
-      cookbook "libvirt"
-      source   "interface_device.xml"
-      variables(
-        :type        => new_resource.type,
-        :mac         => new_resource.mac,
-        :source      => new_resource.source,
-        :model       => new_resource.model,
-        :virtualport => new_resource.virtualport
-      )
-      action :nothing
-    end
-    t.run_action(:create)
+  if device_exist?
+    begin
+      domain = load_domain
+      device_xml = Tempfile.new(new_resource.name)
+      t = template device_xml.path do
+        cookbook "libvirt"
+        source   "interface_device.xml"
+        variables(
+          :type        => new_resource.type,
+          :mac         => new_resource.mac,
+          :source      => new_resource.source,
+          :model       => new_resource.model,
+          :virtualport => new_resource.virtualport
+        )
+        action :nothing
+      end
+      t.run_action(:create)
 
-    domain.attach_device(::File.read(device_xml.path)) rescue nil
-    new_resource.updated_by_last_action(true)
-  rescue Libvirt::RetrieveError
-    raise "You have to define libvirt domain '#{new_resource.domain}' first"
+      domain.attach_device(::File.read(device_xml.path)) rescue nil
+      new_resource.updated_by_last_action(true)
+    rescue Libvirt::RetrieveError
+      raise "You have to define libvirt domain '#{new_resource.domain}' first"
+    end
   end
 end
 
 private
 
-def device_exist?(name)
+def device_exist?
   cmdstr = "virsh domiflist #{new_resource.domain}"
   cmdstr << "|grep #{new_resource.type}" if new_resource.type
   cmdstr << "|grep #{new_resource.source}" if new_resource.source
